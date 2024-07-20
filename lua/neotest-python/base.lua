002d63f88c7e5a511d8254fd94dad03a0d849e3d
tony csoka <tony.csoka@gmail.com>
2024-07-20 12:48:58 +0100

    changed Windows to WindowsNT

--- lua/neotest-python/base.lua
+++ lua/neotest-python/base.lua
@@ -30,7 +30,7 @@ function M.get_python_command(root)
   end
   -- Use activated virtualenv.
   if vim.env.VIRTUAL_ENV then
-    if vim.loop.os_uname().sysname == "Windows" then
+    if vim.loop.os_uname().sysname == "Windows_NT" then
       python_command_mem[root] = { Path:new(vim.env.VIRTUAL_ENV, "Scripts", "python.exe").filename }
       return python_command_mem[root]
     end
